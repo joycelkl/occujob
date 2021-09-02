c0311152b654779200209d8d78aa7773048f64c6
joycelkl <joycelkl@hotmail.com>
2021-09-02 19:07:01 +0800

    added ProfileImage and Redux folder template

---
+++ react_capstone/src/Redux/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+// import linksReducer from './linksReducer' (example)
+
+const reducers = combineReducers({
+    // link: linksReducer (example)
+})
+
+export default reducers
