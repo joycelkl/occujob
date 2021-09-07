085cb438b936e939e3c13a8924f44598957196b3
Zach Lalwani <zach.lalwani@gmail.com>
2021-09-07 16:35:58 +0800

    new login and sign up

--- react_capstone/src/Components/LoginForm.js
+++ react_capstone/src/Components/LoginForm.js
@@ -1,30 +1,32 @@
 import React from "react";
-import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 
 const Login = (props) => {
- 
-  const {onEmailChange, onPasswordChange, handleLogin, email, password} = props
+
+  const { onEmailChange, onPasswordChange, handleLogin, email, password } = props
 
 
   return (
-    <div className="container">
+    <div className="container-fluid">
       <div className="row">
-    <Form className="form-group" onSubmit={(e)=>handleLogin(e)}>
-    <FormGroup>
-        <Label for="exampleEmail">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="Email" onChange={(e) => onEmailChange(e.currentTarget.value)}
-          value={email}/>
-      </FormGroup>
-      <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input type="password" name="password" id="examplePassword" placeholder="Password" onChange={(e) => onPasswordChange(e.currentTarget.value)}
-          value={password}/>
-      </FormGroup>
-      
-      <Button type='submit'>Login</Button>
-      
-    </Form>
-    </div>
+        <Form className="form-group" onSubmit={(e) => handleLogin(e)} style={{ width: "100%", padding:"0", marginTop:"20px" }}>
+          <FormGroup>
+            <Label for="exampleEmail">Email</Label>
+            <Input type="email" name="email" id="exampleEmail" placeholder="Email" onChange={(e) => onEmailChange(e.currentTarget.value)}
+              value={email} />
+          </FormGroup>
+          <FormGroup>
+            <Label for="examplePassword">Password</Label>
+            <Input type="password" name="password" id="examplePassword" placeholder="Password" onChange={(e) => onPasswordChange(e.currentTarget.value)}
+              value={password} />
+          </FormGroup>
+
+          <div className="loginButtons" style={{float: "right", marginTop:"40px"}}>
+            <Button type='submit'>Login</Button>
+            <a href={props.link}><Button style={{marginLeft:"10px"}} type="button">Sign Up</Button></a>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 };
--- react_capstone/src/Pages/EmployerPages/employerLogin.js
+++ react_capstone/src/Pages/EmployerPages/employerLogin.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Login from '../../Components/LoginForm'
 import Navbar from '../../../src/Components/Navbar/navbarLogin';
-import {useState, useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { actionCreators } from '../../Redux';
 import { useHistory } from 'react-router';
-
+import logoBackground from '../../Images/logoBackground.png';
+import "../Applicant CSS/applicantLogin.css"
 
 
 const EmployerLogin = () => {
@@ -14,19 +15,19 @@ const EmployerLogin = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
-    const authState = useSelector((state)=>state.auth);
-    const {isAuthenticated, error} = authState
+    const authState = useSelector((state) => state.auth);
+    const { isAuthenticated, error } = authState
     const history = useHistory();
 
     const dispatch = useDispatch();
 
-    const {loginERuserThunkAction} = bindActionCreators(actionCreators, dispatch)
+    const { loginERuserThunkAction } = bindActionCreators(actionCreators, dispatch)
 
-    useEffect(()=>{
+    useEffect(() => {
         if (isAuthenticated) {
             history.push('/employerHomePage')
         }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isAuthenticated]);
 
     function validEmail(email) {
@@ -34,15 +35,15 @@ const EmployerLogin = () => {
         return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
     }
 
-    function handleLogin (e) {
+    function handleLogin(e) {
         e.preventDefault();
         console.log('ER handle Login')
-        if (!email || !password ) {
+        if (!email || !password) {
             alert('Please input login Email & Password')
             return;
         }
 
-        if(!validEmail(email)) {
+        if (!validEmail(email)) {
             alert('Please input valid Email')
         }
 
@@ -51,25 +52,29 @@ const EmployerLogin = () => {
     }
 
 
-return(
-    <div>
-    <Navbar />
-    <div className="container d-flex">
-    <div className="container">
-    <h1>Company Login Page</h1>
-    <Login onEmailChange={(v)=>setEmail(v)} onPasswordChange={(v)=>setPassword(v)} handleLogin={(e)=>handleLogin(e)} email={email} password={password} />
-    {error && alert(error)}
+    return (
+        <div>
+            <Navbar />
+            <div className="LoginHeader">
+                <div className="container-fluid d-flex">
+                    <div className="container" style={{ border: "1px solid black", padding: "80px", backgroundColor: "rgb(59, 105, 121)", marginTop: "150px", marginLeft: "100px", marginRight: "100px", borderRadius: "25px", color: "white" }}>
+                        <h1 >Employer Login Page</h1>
+                        <Login link="/employerSignup" onEmailChange={(v) => setEmail(v)} onPasswordChange={(v) => setPassword(v)} s={(e) => handleLogin(e)} email={email} password={password} />
+                        {error && alert(error)}
 
-   
-    <a href="/employerSignup">SignUp</a>
-    </div>
-        <div className="container">
-            <h3> Free Lancer testing para</h3>
+
+                    </div>
+                    <div className="container">
+                        <div style={{ marginTop: "220px"}}>
+                            <h1 style={{fontSize:"55px"}}>Ready To Find Your Next Applicant?</h1>
+                            <p style={{fontSize:"28px", lineHeight:"40px"}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta quae neque quia maxime quos
+                                delectus, eveniet asperiores in possimus nisi eius non. Illo quod enim hic fuga quas iure eius?</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-  
-</div>
-)
+    )
 };
 
 export default EmployerLogin;
