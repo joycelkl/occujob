a95cfbd4779382d7f2f2eb7293700ac502715189
Zach Lalwani <zach.lalwani@gmail.com>
2021-09-13 18:16:17 +0800

    new profile page toggle list with props

---
+++ react_capstone/src/Components/PortfolioTable.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, FormGroup, Label, Input, Table, Button } from 'reactstrap';
+import "../Pages/EmployerPages/employerProfilePage.css"
+
+
+
+const ApplicantPortfolioTable = (props) => {
+    return (
+        <div>
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                  <li class="nav-item">
+                    {/* {show ? <h1>Hello World </h1> : null} */}
+                    <p class="nav-link active" id="home-tab" onClick={props.aboutHandler} style={{cursor:"pointer"}}>About</p>
+                  </li>
+                  <li class="nav-item">
+                    <p class="nav-link active" id="home-tab" data-toggle="tab" onClick={props.contactHandler} style={{cursor:"pointer"}}>Contact</p>
+                  </li>
+                  <li class="nav-item">
+                    <p class="nav-link active" id="home-tab" data-toggle="tab" onClick={props.jobPreferenceHandler} style={{cursor:"pointer"}}>Job Preferences</p>
+                  </li>
+                  <li class="nav-item">
+                    <p class="nav-link active" id="home-tab" data-toggle="tab" onClick={props.portfolioHandler} style={{cursor:"pointer"}}>Portfolio</p>
+                  </li>
+
+                </ul>
+        </div>
+    )
+}
+
+export default ApplicantPortfolioTable;
