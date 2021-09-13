467c66cbac503909ffe120468b18fb1c7f35dfb6
joycelkl <joycelkl@hotmail.com>
2021-09-13 16:22:15 +0800

    EE Offer List + reducer + action

--- react_capstone/src/Components/Applicants/ApplicantOfferCard.js
+++ react_capstone/src/Components/Applicants/ApplicantOfferCard.js
@@ -11,12 +11,12 @@ import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 const ApplicantOfferCard = (props)=>{
     const dispatch = useDispatch();
     const { offerAcceptAction } = bindActionCreators(actionCreators, dispatch)
+    const { offerDeclineAction } = bindActionCreators(actionCreators, dispatch)
     
-    
-    const {offerCard, key} = props;
-    console.log(key)
-
+    const {offerCard} = props;
+  
     const {reply,job_title,job_id, er_name, created_at, job_type, job_location, er_img_data,offer,job_description,work_period, expect_salary, req_exp, application_id} = offerCard;
+    
     console.log("testoffer",offer)
     console.log('data', job_title,job_id, er_name, created_at, job_type, job_location, er_img_data,offer,job_description,work_period, expect_salary, req_exp, application_id)
     const [modal, setModal] = useState(false);
@@ -34,15 +34,27 @@ const ApplicantOfferCard = (props)=>{
     //         console.log("pubulic job load err res", err.response)
     //     })
     // }
-    async function declineoffer(application_id){
-        console.log("offer app ID",application_id)
-        const authAxiosConfig = await authAxios();
-        return await authAxiosConfig.post(`/employee/offer/decline/${application_id}`)
-        .then(res => {
-           console.log(res)
-        }).catch(err => {
-            console.log("pubulic job load err res", err.response)
-        })
+
+ 
+    // async function declineoffer(application_id){
+    //     console.log("offer app ID",application_id)
+    //     const authAxiosConfig = await authAxios();
+    //     return await authAxiosConfig.post(`/employee/offer/decline/${application_id}`)
+    //     .then(res => {
+    //        console.log(res)
+    //     }).catch(err => {
+    //         console.log("pubulic job load err res", err.response)
+    //     })
+    // }
+
+    function handleAccept(){
+        console.log('application id', application_id)
+        offerAcceptAction(application_id)
+    }
+
+    function handleDecline(){
+        console.log('application id', application_id)
+        offerDeclineAction(application_id)
     }
 
 //date format
@@ -81,8 +93,8 @@ const ApplicantOfferCard = (props)=>{
         Required Exp: {req_exp}<br/>
         </ModalBody>
         <ModalFooter>
-            {reply === null ? <Button color="primary" onClick={()=>offerAcceptAction(application_id)}>accept</Button> : null }
-            {reply === null ? <Button color="primary" onClick={()=>declineoffer(application_id)}>decline</Button> : null }
+            {reply === null  && offer ? <Button color="primary" onClick={()=>handleAccept()}>accept</Button> : null }
+            {reply === null && offer? <Button color="primary" onClick={()=>handleDecline()}>decline</Button> : null }
           <Button color="secondary" onClick={toggle}>Cancel</Button>
         </ModalFooter>
       </Modal>
