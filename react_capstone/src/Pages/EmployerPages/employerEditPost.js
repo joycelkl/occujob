3d9e9ad0c0cbb2524db28bc6809e90effe0da5e2
Zach Lalwani <zach.lalwani@gmail.com>
2021-09-15 17:39:25 +0800

    all rating post and recieve stars and comments

--- react_capstone/src/Pages/EmployerPages/employerEditPost.js
+++ react_capstone/src/Pages/EmployerPages/employerEditPost.js
@@ -9,10 +9,11 @@ import { useHistory } from 'react-router';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import "./modalFullPage.css";
+import ControlledRating from "../../Components/Rating/ControlledRating";
 
 
 const EmployerEditPost = () => {
-    
+
     const offerToast = () => toast("An Offer Has Been Made")
     const fillInfoToast = () => toast("Please Fill In All Information")
     const indJobState = useSelector((state) => state.individualJob)
@@ -21,52 +22,67 @@ const EmployerEditPost = () => {
 
     const dispatch = useDispatch()
     const { erJobUpdate } = bindActionCreators
-    (actionCreators, dispatch)
+        (actionCreators, dispatch)
     const { loadLocationThunkAction } = bindActionCreators(actionCreators, dispatch)
     const { loadIndustryThunkAction } = bindActionCreators(actionCreators, dispatch)
 
     useEffect(() => {
         loadLocationThunkAction();
-      loadIndustryThunkAction();
-      // eslint-disable-next-line react-hooks/exhaustive-deps
+        loadIndustryThunkAction();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const locationState = useSelector((state) => state.location);
     console.log("location", locationState)
     const industryState = useSelector((state) => state.industry);
     console.log("industry", industryState)
-    
-    const { job_id, expect_salary, job_title, job_location, job_type, req_exp, job_description, status, expiry_date, jobCreate, job_function, work_period, application_id, job_salary_type, reply, offer } = indJobState[0] || {}
+
+    const { job_id, expect_salary, job_title, job_location, job_type, req_exp, job_description, status, expiry_date, jobCreate, job_function, work_period, application_id, job_salary_type, reply, offer, ee_id } = indJobState[0] || {}
 
     const history = useHistory();
 
     console.log('data', job_id, expect_salary, job_title, job_location, job_type, req_exp, job_description, status, expiry_date, jobCreate, job_function, work_period, reply)
 
-    const [salary, setSalary] = useState(expect_salary );
+    const [salary, setSalary] = useState(expect_salary);
     const [jobTitle, setJobTitle] = useState(job_title);
-    const [jobLocation, setJobLocation] = useState(job_location );
-    const [empType, setEmpType] = useState(job_type );
-    const [reqExp, setReqExp] = useState(req_exp );
-    const [jobDes, setJobDes] = useState(job_description );
-    const [jobStatus, setJobStatus] = useState(status );
+    const [jobLocation, setJobLocation] = useState(job_location);
+    const [empType, setEmpType] = useState(job_type);
+    const [reqExp, setReqExp] = useState(req_exp);
+    const [jobDes, setJobDes] = useState(job_description);
+    const [jobStatus, setJobStatus] = useState(status);
     const [jobFunction, setJobFunction] = useState(job_function);
     const [workPeriod, setWorkPeriod] = useState(work_period);
-    const[salaryType, setSalaryType] = useState(job_salary_type);
+    const [salaryType, setSalaryType] = useState(job_salary_type);
     const [modal, setModal] = useState(false);
     const [modalJob, setModalJob] = useState({});
     const [offering, setoffering] = useState(offer);
+    const [nestedModal, setNestedModal] = useState(false);
+    const [closeAll, setCloseAll] = useState(false);
+    const [rating, setRating] = useState(0)
+    const toggle = () => setModal(!modal);
+    const [comment, setComment] = useState('')
 
-
-    function toggle(name) {
-        console.log('toggle name', name)
-        
-        setModal(!modal);
+    console.log("RATING PASSED FROM CHILD", rating)
+    const toggleNested = () => {
+        setNestedModal(!nestedModal);
+        setCloseAll(false);
     }
+
+    const toggleAll = () => {
+        setNestedModal(!nestedModal);
+        setCloseAll(true);
+    }
+
+    // function toggle(name) {
+    //     console.log('toggle name', name)
+
+    //     setModal(!modal);
+    // }
 
     function handleRepost(e) {
         e.preventDefault();
         console.log('repost')
-        if (!jobTitle || !jobDes || !reqExp || !salary || !empType || !jobFunction || !jobLocation || !workPeriod ) {
+        if (!jobTitle || !jobDes || !reqExp || !salary || !empType || !jobFunction || !jobLocation || !workPeriod) {
             fillInfoToast();
             console.log('submitting')
             return;
@@ -75,6 +91,23 @@ const EmployerEditPost = () => {
             jobDes, workPeriod, jobLocation, empType, salaryType).then(() => {
                 history.push('/employerJobRecordsList')
             })
+    }
+
+
+
+    async function applicantRating(ee_id, application_id, rating, comment) {
+        const authAxiosConfig = await authAxios();
+        return await authAxiosConfig.post('/employer/CompanyGiveRating', {
+            ee_id: ee_id,
+            application_id: application_id,
+            rate: rating,
+            comment: comment,
+
+        }).then(res => {
+            console.log("POST SUCCESS", res)
+        }).catch(err => {
+            console.log("job posting err res", err.response)
+        })
     }
 
     async function erJobCreate(jobTitle, jobFunction, reqExp, expectSalary,
@@ -99,28 +132,28 @@ const EmployerEditPost = () => {
 
     function handleUpdate(e) {
         e.preventDefault();
-        if (!jobTitle || !jobDes || !reqExp || !salary || !empType || !jobFunction || !jobLocation || !workPeriod ) {
+        if (!jobTitle || !jobDes || !reqExp || !salary || !empType || !jobFunction || !jobLocation || !workPeriod) {
             fillInfoToast();
             return;
         }
-        erJobUpdate(job_id, jobTitle, jobFunction, reqExp, salary, jobDes, workPeriod, jobStatus, jobLocation, empType, salaryType).then(()=>{
-                alert("updated")
-                history.push('/employerJobRecordsList')
-            })
+        erJobUpdate(job_id, jobTitle, jobFunction, reqExp, salary, jobDes, workPeriod, jobStatus, jobLocation, empType, salaryType).then(() => {
+            alert("updated")
+            history.push('/employerJobRecordsList')
+        })
     }
 
-    async function giveOffer(application_id){
-        console.log("offer app ID",application_id)
+    async function giveOffer(application_id) {
+        console.log("offer app ID", application_id)
         const authAxiosConfig = await authAxios();
         return await authAxiosConfig.post(`/employer/job/candidate/offer/${application_id}`)
-        .then(res => {
-            offerToast()
-            setoffering(true)
-           console.log(res)
-           setModal(!modal);
-        }).catch(err => {
-            console.log("pubulic job load err res", err.response)
-        })
+            .then(res => {
+                offerToast()
+                setoffering(true)
+                console.log(res)
+                setModal(!modal);
+            }).catch(err => {
+                console.log("pubulic job load err res", err.response)
+            })
     }
 
     function handleCheckbokChange(e) {
@@ -152,12 +185,12 @@ const EmployerEditPost = () => {
                         <h5>Expiry Date: {day + "/" + month + "/" + year}</h5>
                         <div>
                             <h5>Job Status: {status ? 'Active' : 'Inactive'}</h5>
-                            {status? (<FormGroup check>
+                            {status ? (<FormGroup check>
                                 <Label check>
                                     <Input type="checkbox" onChange={(e) => handleCheckbokChange(e)} />{' '}
                                     Deactivate Job Post
                                 </Label>
-                            </FormGroup>):null}
+                            </FormGroup>) : null}
                         </div>
                     </div>
                     <Form className='form-group' onSubmit={(e) => handleUpdate(e)}>
@@ -193,12 +226,12 @@ const EmployerEditPost = () => {
                         </FormGroup>
                         <FormGroup>
                             <Label for="JobFunction">Job Function</Label>
-                            <Input type="select" name="JobFunction" id="JobFunction"  value={jobFunction} onChange={(e) => setJobFunction(e.target.value)}>
-                          <option value={null} selected>Please select</option>
-                          {industryState.length > 0 ? industryState.map((industry, i) => (
-                            <option key={i} value={industry.industry} selected>{industry.industry}</option>
-                          )) : "loading..."}
-                          </Input>
+                            <Input type="select" name="JobFunction" id="JobFunction" value={jobFunction} onChange={(e) => setJobFunction(e.target.value)}>
+                                <option value={null} selected>Please select</option>
+                                {industryState.length > 0 ? industryState.map((industry, i) => (
+                                    <option key={i} value={industry.industry} selected>{industry.industry}</option>
+                                )) : "loading..."}
+                            </Input>
                         </FormGroup>
                         <FormGroup>
                             <Label for="workPeriod">Work Period</Label>
@@ -207,14 +240,14 @@ const EmployerEditPost = () => {
                         <FormGroup>
                             <Label for="preferworklocation">Work Location</Label>
                             <Input type="select" name="location" id="location" placeholder="location" value={jobLocation} onChange={(e) => setJobLocation(e.target.value)}>
-                          <option value={null} selected>Please select</option>
-                          {locationState.length > 0 ? locationState.map((location, i) => (
-                            <option key={i} value={location.location} selected>{location.location}</option>
-                          )) : "loading..."}
+                                <option value={null} selected>Please select</option>
+                                {locationState.length > 0 ? locationState.map((location, i) => (
+                                    <option key={i} value={location.location} selected>{location.location}</option>
+                                )) : "loading..."}
                             </Input>
                         </FormGroup>
                         <div className='row'>
-                            {status?<Button type='submit'>Save Changes</Button>:null}
+                            {status ? <Button type='submit'>Save Changes</Button> : null}
                             <Button onClick={(e) => handleRepost(e)}>Repost</Button>
                             <ToastContainer />
                         </div>
@@ -235,34 +268,50 @@ const EmployerEditPost = () => {
                         {indJobState[0].ee_name ? indJobState.map((job) => (
                             <tr onClick={() => {
                                 setModalJob(job)
-                                toggle(job.ee_name)}
-                            } 
+                                toggle(job.ee_name)
+                            }
+                            }
                                 key={job.ee_id} style={{ cursor: "pointer" }}>
-                            
+
                                 <td>{job.ee_name}</td>
                                 <td>{job.created_at}</td>
-                                {offering? <td> Offer Sent</td> : <td> No Offer</td>}
-                                {job.reply === true? <td>Accepted </td> : (job.reply === false ?<td>Declined</td> : <td> No Reply</td>)} 
+                                {offering ? <td> Offer Sent</td> : <td> No Offer</td>}
+                                {job.reply === true ? <td>Accepted </td> : (job.reply === false ? <td>Declined</td> : <td> No Reply</td>)}
                             </tr>
 
                         )) : "Waiting for Applicant Apply"}
                         <Modal isOpen={modal} toggle={toggle} fade={false} className='modal-sandbox'>
 
-                                    <ModalHeader>{modalJob.ee_name} {console.log('nametest', modalJob.ee_name)} 
-                                    <img src={modalJob.ee_img_data} width="200px" height="200x" alt=''/>
-                                    </ModalHeader>
-                                    <ModalBody className='modal-content'>
-                                        Self Introduction: {modalJob.self_intro} <br />
-                                        Expected Salary: {modalJob.expected_salary} <br />
-                                        Skills: {modalJob.ee_industry} <br />
-                                        Location Preference: {modalJob.ee_location} <br />
-                                        availability: {modalJob.availability}
+                            <ModalHeader>{modalJob.ee_name} {console.log('nametest', modalJob.ee_name)}
+                                <img src={modalJob.ee_img_data} width="200px" height="200x" alt='' />
+                            </ModalHeader>
+                            <ModalBody className='modal-content'>
+                                Self Introduction: {modalJob.self_intro} <br />
+                                Expected Salary: {modalJob.expected_salary} <br />
+                                Skills: {modalJob.ee_industry} <br />
+                                Location Preference: {modalJob.ee_location} <br />
+                                availability: {modalJob.availability}
+                            </ModalBody>
+                            <ModalFooter>
+                                <Button color="primary" onClick={toggle}>Message</Button>{' '}
+                                <Button color="secondary" onClick={() => giveOffer(application_id)}>Offer</Button>
+                                <Button color="success" onClick={toggleNested}>Rate Company</Button>
+                                <Modal isOpen={nestedModal} toggle={toggleNested} onClosed={closeAll ? toggle : undefined} fade={false}>
+                                    <ModalHeader>Ratings</ModalHeader>
+                                    <ModalBody>
+                                        <ControlledRating ratingValue={(value) => setRating(value)} />
+                                    </ModalBody>
+                                    <ModalBody>
+                                        <h1>Comments:</h1>
+                                        <Input style={{ marginTop: "10px" }} type="textarea" name="compDes" id="intro" spellCheck='true' placeholder="Company Description" value={comment} onChange={(e) => setComment(e.target.value)} />
                                     </ModalBody>
                                     <ModalFooter>
-                                        <Button color="primary" onClick={toggle}>Message</Button>{' '}
-                                        <Button color="secondary" onClick={()=>giveOffer(application_id)}>Offer</Button>
+                                        <Button color="primary" onClick={() => applicantRating(ee_id, application_id, rating, comment)}>Rate</Button>{' '}
+                                        <Button color="primary" onClick={toggleNested}>Close</Button>{' '}
                                     </ModalFooter>
                                 </Modal>
+                            </ModalFooter>
+                        </Modal>
                     </tbody>
                 </Table>
 
