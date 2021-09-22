86ac7b74fd7b19a2b7aab4528808022a658952d4
Zach Lalwani <zach.lalwani@gmail.com>
2021-09-22 14:26:47 +0800

    new styles checklist

--- react_capstone/src/Pages/ApplicantPages/applicantChatMessages.jsx
+++ react_capstone/src/Pages/ApplicantPages/applicantChatMessages.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
-import AllChatrooms from '../../Components/Chatroom/AllChatrooms'
-import ApplicantNavbar from '../../Components/Navbar/navbarApplicant';
+import React from "react";
+import AllChatrooms from "../../Components/Chatroom/AllChatrooms";
+import ApplicantNavbar from "../../Components/Navbar/navbarApplicant";
+import "./applicantChatMessages.css";
 
 const ApplicantChatMessages = () => {
-    return (
-        <div>
-            <ApplicantNavbar />
-            <div className='container'>
-                 <AllChatrooms />
-            </div>
+  return (
+    <div>
+      <ApplicantNavbar />
+      <section className="chatListHeader">
+        <div className="chatListText-box" id="home">
+          <h1>Messages</h1>
+          <p>Chat With Your Future Employer!</p>
+          <a href="/applicantJobSearch" className="Homebtn">
+            Search Jobs
+          </a>
         </div>
-    )
-}
+      </section>
+      <div className="container">
+        <AllChatrooms />
+      </div>
+    </div>
+  );
+};
 
-export default ApplicantChatMessages
+export default ApplicantChatMessages;
--- react_capstone/src/Pages/EmployerPages/employerChatMessages.jsx
+++ react_capstone/src/Pages/EmployerPages/employerChatMessages.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import AllChatrooms from '../../Components/Chatroom/AllChatrooms'
+import React from "react";
+import AllChatrooms from "../../Components/Chatroom/AllChatrooms";
 import EmployerNavbar from "../../Components/Navbar/navbarEmployer";
-
+import "../ApplicantPages/applicantChatMessages.css";
 
 const EmployerChatMessages = () => {
-    return (
-        <div>
-            <EmployerNavbar />
-            <div className='container'>
-                 <AllChatrooms />
-            </div>
+  return (
+    <div>
+      <EmployerNavbar />
+      <section className="chatListHeader">
+        <div className="chatListText-box" id="home">
+          <h1>Messages</h1>
+          <p>Chat With Your Future Employer!</p>
+          <a href="/applicantJobSearch" className="Homebtn">
+            Search Jobs
+          </a>
         </div>
-    )
-}
+      </section>
+      <div className="container">
+        <AllChatrooms />
+      </div>
+    </div>
+  );
+};
 
-export default EmployerChatMessages
+export default EmployerChatMessages;
